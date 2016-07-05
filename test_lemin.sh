76f57f786e137086d01ecf11941190493345d43b
Alain DEMENET <ademenet@student.42.fr>
2016-07-05 18:33:09 +0200

    Update

--- test_lemin.sh
+++ test_lemin.sh
@@ -16,7 +16,7 @@ test_errors1()
 
 test_errors2()
 {
-	for i in {25..48}
+	for i in {25..49}
 	do
 		printf "\033[34;1m./lem-in < test/test_err%d.map\n\033[0m" $i
 		./lem-in < test/test_err$i.map
@@ -36,7 +36,7 @@ test_valid1()
 
 test_valid2()
 {
-	for i in {16..30}
+	for i in {16..31}
 	do
 		printf "\033[34;1m./lem-in < test/test_%d.map\n\033[0m" $i
 		./lem-in < test/test_$i.map
@@ -67,9 +67,9 @@ fi
 while true; do
 	echo "\033[34;1mChoose a test:\n
 		[1]\tValids from 1 to 15\n
-		[2]\tValids from 16 to 30\n
+		[2]\tValids from 16 to 31\n
 		[3]\tErrors from 1 to 24\n
-		[4]\tErrors from 25 to 48\n
+		[4]\tErrors from 25 to 49\n
 		[5-i]\tBig with i from 1 to 5\n
 		[b]\tSee bonuses\n
 		[q]\tQuit\n
